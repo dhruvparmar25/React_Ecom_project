ded4505d3fa3ae756c8e243f7b518583f807e7b3
Dhruv Parmar <162405820+dhruvparmar25@users.noreply.github.com>
2025-09-17 16:34:21 +0530

    Delete fashion_store directory

--- fashion_store/fashion_store_be-main/routes/categoryRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getCategories, getAll } = require("../controllers/categoryController");
-
-const categoryRouter = Router();
-
-
-categoryRouter.get("/category",getAll, getCategories);
-
-module.exports = categoryRouter;
